637c10c9037627b0bf360e63251ac0db2fe93c35
amefure <shachi10969@gmail.com>
2025-07-05 22:10:12 +0900

    Initial Commit

---
+++ CapsuleToyApp/CapsuleToyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapsuleToyAppApp.swift
+//  CapsuleToyApp
+//
+//  Created by t&a on 2025/07/05.
+//
+
+import SwiftUI
+
+@main
+struct CapsuleToyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
