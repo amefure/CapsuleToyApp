873b80f3f534292c740063a5727aa3c66ba28c77
amefure <shachi10969@gmail.com>
2025-07-05 22:15:45 +0900

    初期プロジェクトセットアップ

--- CapsuleToyApp/View/RootView.swift
+++ CapsuleToyApp/View/RootView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  RootView.swift
 //  CapsuleToyApp
 //
 //  Created by t&a on 2025/07/05.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RootView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    RootView()
 }
