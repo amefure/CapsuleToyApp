22b0b37a6df314c3362814c5dde373023b0306e2
amefure <shachi10969@gmail.com>
2025-08-05 23:36:45 +0900

    RealmListオブジェクトの更新処理の実装

--- CapsuleToyApp/ViewModel/Series/SeriesEntryViewModel.swift
+++ CapsuleToyApp/ViewModel/Series/SeriesEntryViewModel.swift
@@ -83,10 +83,6 @@ extension SeriesEntryViewModel {
             return
         }
         
-        let realmLocations = locations.toRealmList()
-        
-        let realmCategories = categories.toRealmList()
-        
         if let id {
             // 画像が存在すれば保存してパスを渡す
             let path: String? = saveImageForLocal(id: id.stringValue, image: image)
@@ -96,14 +92,87 @@ extension SeriesEntryViewModel {
                 series.count = count
                 series.amount = amount
                 series.memo = memo
-                series.locations = realmLocations
-                series.categories = realmCategories
+                
+                // Realmオブジェクトのプロパティにリストで保持している場合はそのまま更新しようとすると
+                // Object is already managed by another Realm. Use create instead to copy it into this Realm.となるため
+                // series.locations = realmLocations ×
+                // 以下のように明示的に更新・追加・削除を行う
+                if locations.isEmpty {
+                    // 変更対象が存在しない場合は明示的に空の配列を格納してリセット
+                    series.locations = RealmSwift.List<Location>()
+                } else {
+                    // (1)既存のものがリストからなくなっている => 削除
+                    // (2)既存のもの内容が更新されている      => 更新
+                    // (3)既存のものに同じIDのものが存在しない => 追加
+                    
+                    // (1)既存のものがリストからなくなっている => 削除
+                    let toRemove = series.locations.filter { location in
+                        // Realmの比較は参照比較なので同じRelamインスタンスに属した同じものでないと一致しない
+                        // そのためidなどで明示的に比較する必要がある
+                        !categories.contains(where: { $0.id == location.id })
+                    }
+                    toRemove.forEach { location in
+                        guard let index = series.locations.firstIndex(where: { $0.id == location.id }) else { return }
+                        series.locations.remove(at: index)
+                    }
+                    
+                    
+                    locations.forEach { newLocation in
+                        if let location = series.locations.first(where: { $0.id == newLocation.id }) {
+                            // (2)既存のもの内容が更新されている      => 更新
+                            location.name = newLocation.name
+                            location.latitude = newLocation.latitude
+                            location.longitude = newLocation.longitude
+                        } else {
+                            // (3)既存のものに同じIDのものが存在しない => 追加
+                            series.locations.append(newLocation)
+                        }
+                    }
+                }
+                
+                
+                // 変更対象のが存在しない場合は明示的に空の配列を格納してリセット
+                if categories.isEmpty {
+                    series.categories = RealmSwift.List<Category>()
+                } else {
+                    // (1)既存のものがリストからなくなっている => 削除
+                    // (2)既存のもの内容が更新されている      => 更新
+                    // (3)既存のものに同じIDのものが存在しない => 追加
+                    
+                    // (1)既存のものがリストからなくなっている => 削除
+                    let toRemove = series.categories.filter { category in
+                        // Realmの比較は参照比較なので同じRelamインスタンスに属した同じものでないと一致しない
+                        // そのためidなどで明示的に比較する必要がある
+                        !categories.contains(where: { $0.id == category.id })
+                    }
+                    toRemove.forEach { category in
+                        guard let index = series.categories.firstIndex(where: { $0.id == category.id }) else { return }
+                        series.categories.remove(at: index)
+                    }
+
+                    
+                    categories.forEach { newCategory in
+                        if let category = series.categories.first(where: { $0.id == newCategory.id }) {
+                            // (2)既存のもの内容が更新されている      => 更新
+                            category.name = newCategory.name
+                            category.colorHex = newCategory.colorHex
+                        } else {
+                            // (3)既存のものに同じIDのものが存在しない => 追加
+                            series.categories.append(newCategory)
+                        }
+                    }
+                }
+                
                 series.imagePath = path
                 series.createdAt = Date()
                 series.updatedAt = Date()
                 showUpdateSuccessAlert = true
             }
         } else {
+            
+            let realmLocations = locations.toRealmList()
+            let realmCategories = categories.toRealmList()
+            
             let series = Series()
             // 画像が存在すれば保存してパスを渡す
             let path: String? = saveImageForLocal(id: series.id.stringValue, image: image)
