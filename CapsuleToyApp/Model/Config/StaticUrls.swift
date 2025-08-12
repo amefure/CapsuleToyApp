a5330b371f07bedcfdaece55c2efe0ffc1f6156b
amefure <shachi10969@gmail.com>
2025-08-12 22:15:31 +0900

    リリースバージョンインクリメント

--- CapsuleToyApp/Model/Config/StaticUrls.swift
+++ CapsuleToyApp/Model/Config/StaticUrls.swift
@@ -7,7 +7,7 @@
 
 final class StaticUrls: Sendable {
     /// アプリURL
-    static let APP_URL = "https://apps.apple.com/jp/app/%E3%81%BF%E3%82%93%E3%81%AA%E3%81%AE%E5%87%BA%E7%94%A3%E7%A5%9D%E3%81%84/id6742353345"
+    static let APP_URL = "https://apps.apple.com/jp/app/%E3%82%AC%E3%83%81%E3%83%A3%E3%82%B3%E3%83%AC-%E3%82%AB%E3%83%97%E3%82%BB%E3%83%AB%E3%83%88%E3%82%A4%E8%A8%98%E9%8C%B2%E3%82%A2%E3%83%97%E3%83%AA/id6749797762"
     /// レビュー
     static let APP_REVIEW_URL = APP_URL + "?action=write-review"
     /// Webサイトルート
